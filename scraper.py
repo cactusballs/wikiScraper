23b2b95873f31acfcb0ce87f20b7401d167b869f
cactusballs <lottielucyjudge@gmail.com>
2025-02-27 19:18:02 +0000

    switching to new branch cos good practice

--- scraper.py
+++ scraper.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-  
+
+#getting the response from the desired webste 
 response = requests.get(
     url="https://en.wikipedia.org/wiki/Web_scraping",
 )
+
+#parsing the HTML content of the web page using beut soup making it easier to navigate data as it gives you it structured
+soup = BeautifulSoup(response.content, 'html.parser')
+
+#finding element by the IDtag using soup
+
+
 print(response.status_code)
+
